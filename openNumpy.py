efce5d5c5d832c638e0a6ba3a6db184a1c83058e
Sadia Khalil <skhalil@phsx-cms.home.ku.edu>
2019-01-09 12:20:31 -0600

    update the scripts such as avoid running over all the lumi maps for every data run

---
+++ openNumpy.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import os, sys
+import numpy as np
+
+data = np.load('lumiarray.npy')
+
+#print data
+RUN = '317683'
+
+
+#for i, row in enumerate(data):
+#    print(row[0], row[1], row[2], row[3])
+#    if str(row[0])[0:6] != RUN: np.delete(data, i, 0)
+    
+indices = [i for (i, row) in  enumerate(data) if str(row[0])[0:6] != RUN]
+print('Keeping {0:<3.0f} rows out of {1:<3.0f} rows for RUN {2:s}' ).format((data.shape[0] - len(indices)),  data.shape[0], RUN)
+b = np.delete(data, indices, 0)
+print b
+np.save('lumiMap_'+RUN+'.npy', b)
+
+#unmatched = [i for (i,v) in enumerate(    
+#for x in np.nditer(data):
+#    print x   
+
+#for (x, y), value in np.ndenumerate(data):
+#    print x,y
+
